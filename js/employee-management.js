1f6c1cf1ef8a77361a0ebecddceccecc90536b6b
Donnyves Laroque <40510674+laroqued@users.noreply.github.com>
2022-10-22 16:38:30 -0700

    Wired up add button to output message to console

--- js/employee-management.js
+++ js/employee-management.js
@@ -1,4 +1,20 @@
 /*eslint-env browser*/
 
+var $ = function (id) {
+  "use strict";
+  return document.getElementById(id);
+};
 
+var processEntries = function () {
+  "use strict";
+  console.log(`Add Row clicked`);
 
+  var header, html, required, msg, name, title, email;
+};
+
+window.addEventListener("load", function () {
+  "use strict";
+  $("add_row").addEventListener("click", processEntries);
+
+  $("name_input").focus();
+});
